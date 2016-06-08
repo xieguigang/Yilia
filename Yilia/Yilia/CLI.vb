2d2316636840a8dfcd3614cf67a0a6da818a45b0
xieguigang <xie.guigang@gmail.com>
2016-06-08 22:02:57 +0800

    add test theme and parser for the article meta success

--- Yilia/Yilia/CLI.vb
+++ Yilia/Yilia/CLI.vb
@@ -3,6 +3,7 @@ Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.MarkupLanguage
 Imports Microsoft.VisualBasic.Language.UnixBash
 Imports Yilia.Config
+Imports System.Text
 
 Module CLI
 
@@ -20,9 +21,11 @@ Module CLI
         For Each file As String In ls - l - r - wildcards("*.md") <= (App.CurrentDirectory & "/" & config.source_dir)
             Dim md As String = file.GET()
             Dim html As String = engine.HTML(md)
-            Dim rel As String = ProgramPathSearchTool.RelativePath(file, App.CurrentDirectory)
-            Dim out As String = $"{App.CurrentDirectory}/{config.public_dir}/{file.ParentPath}/index.html"
-            Call html.SaveTo(out)
+            Dim rel As String = ProgramPathSearchTool.RelativePath(App.CurrentDirectory & "/" & config.source_dir & "/", file)
+            rel = rel.Replace("..\", "")
+            rel = rel.Replace("../", "")
+            Dim out As String = $"{App.CurrentDirectory}/{config.public_dir}/{rel.ParentPath(False)}/index.html"
+            Call html.SaveTo(out, Encoding.UTF8)
         Next
 
         Return 0
--- Yilia/Yilia/MarkdownGenerate.vb
+++ Yilia/Yilia/MarkdownGenerate.vb
@@ -1,6 +1,10 @@
-﻿Imports Microsoft.VisualBasic.MarkupLanguage
+﻿Imports System.Text
+Imports System.Text.RegularExpressions
+Imports Microsoft.VisualBasic.MarkupLanguage
 Imports Microsoft.VisualBasic.MarkupLanguage.MarkDown
+Imports Microsoft.VisualBasic.Serialization
 Imports Yilia.Config
+Imports Yilia.Db
 
 Public Class MarkdownGenerate
 
@@ -12,7 +16,60 @@ Public Class MarkdownGenerate
         _config = config
     End Sub
 
-    Public Function HTML(markdown As String) As String
+    Public Function ToHTML(markdown As String) As String
+        Dim meta As PostMeta = TryParseMeta(markdown)
+        Dim body As String = _engine.Transform(markdown)
+        Dim html As New StringBuilder(markdown.Length)
 
+        Call html.AppendLine($"<body>
+{body}
+</body>")
+
+        Return html.ToString
+    End Function
+
+    Public Shared Function TryParseMeta(ByRef md As String) As PostMeta
+        Dim head As String = Regex.Match(md, "---.+?\n---", RegexOptions.Singleline).Value
+        md = Mid(md, Len(head) + 1).Trim
+        Return New PostMeta(head)
     End Function
 End Class
+
+Public Structure PostMeta
+    Public Property title As String
+    Public Property [date] As String
+    Public Property source As String
+    Public Property raw As String
+    Public Property slug As String
+    Public Property layout As String
+    Public Property photos As String
+    Public Property link As String
+
+    Sub New(s As String)
+        Dim lines As String() = s.lTokens
+        Dim hash As New Dictionary(Of String, String)
+
+        For i As Integer = 1 To lines.Length - 2
+            Dim line As String = lines(i)
+            Dim value = line.GetTagValue(":")
+            hash.Add(Trim(value.Name).ToLower, Trim(value.x))
+        Next
+
+        VBDebugger.Mute = True
+
+        title = hash.TryGetValue(NameOf(title))
+        [date] = hash.TryGetValue(NameOf([date]))
+        source = hash.TryGetValue(NameOf(source))
+        raw = hash.TryGetValue(NameOf(raw))
+        slug = hash.TryGetValue(NameOf(slug))
+        layout = hash.TryGetValue(NameOf(layout))
+        photos = hash.TryGetValue(NameOf(photos))
+        link = hash.TryGetValue(NameOf(link))
+
+        VBDebugger.Mute = False
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return Me.GetJson
+    End Function
+End Structure
