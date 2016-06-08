d4600b1e9311cc49571b899a0022a26347473e03
xieguigang <xie.guigang@gmail.com>
2016-06-08 17:21:50 +0800

    basic server framework

---
+++ Yilia/Yilia/CLI.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+
+Module CLI
+
+    <ExportAPI("/server")>
+    Public Function RunServer(args As CommandLine) As Integer
+
+    End Function
+
+    <ExportAPI("/generate")>
+    Public Function Generate(args As CommandLine) As Integer
+
+    End Function
+End Module
---
+++ Yilia/Yilia/MarkdownGenerate.vb
@@ -0,0 +1,3 @@
+﻿Public Class MarkdownGenerate
+
+End Class
