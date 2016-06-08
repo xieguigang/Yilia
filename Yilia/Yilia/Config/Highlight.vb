64b0d9c435daa8faa9100b3a68d76ac63d28f66e
xieguigang <xie.guigang@gmail.com>
2016-06-08 17:47:09 +0800

    add configs

---
+++ Yilia/Yilia/Config/Highlight.vb
@@ -0,0 +1,6 @@
+﻿Public Class Highlight
+    Public Property enable As Boolean
+    Public Property line_number As Boolean
+    Public Property auto_detect As Boolean
+    Public Property tab_replace As String
+End Class
