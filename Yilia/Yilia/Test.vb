2625375f736c3325843cfa0010a810301a11194d
xieguigang <xie.guigang@gcmodeller.org>
2017-11-19 14:22:09 +0800

    add test

---
+++ Yilia/Yilia/Test.vb
@@ -0,0 +1,10 @@
+﻿Imports SMRUCC.WebCloud.VBScript
+
+Module Test
+
+    Sub Main()
+        Dim post As New PostMeta
+        Dim html$ = vbhtml.ReadHTML("G:\repo\Yilia\Yilia\site_demo", "G:\repo\Yilia\Yilia\site_demo\pages\index.vbhtml", New Dictionary(Of String, Object) From {{"post", {post}}})
+        Call html.SaveTo("./test.html")
+    End Sub
+End Module
