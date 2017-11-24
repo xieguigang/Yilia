47f0ca92654eaa7628eb93da29c02faa462f984c
xieguigang <xie.guigang@gcmodeller.org>
2017-11-24 21:35:22 +0800

    add hello world post for test

--- Yilia/Yilia/Test.vb
+++ Yilia/Yilia/Test.vb
@@ -3,7 +3,10 @@
 Module Test
 
     Sub Main()
-        Dim post As New PostMeta
+        Dim post As PostMeta = PostMeta.FromMarkdownFile("G:\repo\Yilia\Yilia\site_demo\post\hello_world.md")
+
+
+
         Dim html$ = vbhtml.ReadHTML("G:\repo\Yilia\Yilia\site_demo", "G:\repo\Yilia\Yilia\site_demo\pages\index.vbhtml", New Dictionary(Of String, Object) From {{"post", {post}}})
         Call html.SaveTo("./test.html")
     End Sub
---
+++ Yilia/site_demo/post/hello_world.md
@@ -0,0 +1,17 @@
+title: test post
+date: 2017-11-24
+updated: 2017-11-25
+source: http://lilithaf.me
+preview: ./images/1046630947.jpeg
+categories:
+   - Diary
+   - Life
+tags: photo; toy; ACG
+
+---
+
+# 手机摄影
+
+![](./images/1046630947.jpeg)
+
+![](./images/IMG_20171123_124448.jpg)
