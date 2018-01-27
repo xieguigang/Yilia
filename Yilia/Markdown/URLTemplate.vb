53f822b555ac2f88c9b986fddbf5f0754835efab
谢桂纲 <xie.guigang@gcmodeller.org>
2018-01-27 11:31:59 +0800

    add url template object

---
+++ Yilia/Markdown/URLTemplate.vb
@@ -0,0 +1,12 @@
+﻿Public Enum URLTemplates As Byte
+
+    ''' <summary>
+    ''' ``year/month/day/title/index.html``
+    ''' </summary>
+    DateTitle = 0
+    ''' <summary>
+    ''' ``folder/file.md`` -> ``folder/file.html``
+    ''' </summary>
+    SourceLink
+
+End Enum
