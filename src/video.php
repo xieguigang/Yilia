0ee2531040675a3624b7e5a7b7797fa5510751eb
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-22 21:58:58 +0800

    file not found?

--- src/video.php
+++ src/video.php
@@ -5,7 +5,7 @@ include __DIR__ . "/../.etc/bootstrap.php";
 class App {
 
     function __construct() {
-        include APP_PATH . "/frameworks/plugins/SeekableVideo.php";
+        
     }
 
     /**
@@ -51,12 +51,21 @@ class App {
      * @access *
     */
     public function stream($id) {
-        $filepath = VIDEO_UPLOAD . "/Laputa.Castle.in.the.Sky_1.mp4";
-        /**
-         * Common use of a video intermediary: make sure user is allowed to access this video.
-         * <Code necessary checks here>
-        */
-        $video = new SeekableVideo($filepath, 'video/mp4', 'stream.mp4');
-        $video->begin_stream();
+        include APP_PATH . "/frameworks/plugins/SeekableVideo.php";
+
+        $video = new Table("video");
+        $video = $video->where(["id" => $id])->find();
+
+        if (Utils::isDbNull($video)) {
+            RFC7231Error::err404(); 
+        } else {
+            $filepath = VIDEO_UPLOAD . "/" . $video["filepath"];
+            /**
+             * Common use of a video intermediary: make sure user is allowed to access this video.
+             * <Code necessary checks here>
+            */
+            $video = new SeekableVideo($filepath, 'video/mp4', 'stream.mp4');
+            $video->begin_stream();
+        }        
     }
 }
