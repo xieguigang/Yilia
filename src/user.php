740ed054c39f5add91eaaef465684ef0b481e28b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-24 18:20:25 +0800

    user api

---
+++ src/user.php
@@ -0,0 +1,16 @@
+<?php
+
+include __DIR__ . "/../.etc/bootstrap.php";
+
+class App {
+
+    /**
+     * create a new user
+     * 
+     * @uses api
+     * @access *
+    */
+    public function signup() {
+
+    }
+}
