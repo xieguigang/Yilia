8fd82125548c5c886500e9bc8eb6e7077f9a2696
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-24 19:10:38 +0800

    Update user_log.php

--- scripts/user/user_log.php
+++ scripts/user/user_log.php
@@ -10,6 +10,20 @@ class user_log {
      * @param boolean $success the user login success or error?
     */
     public static function log($email, $user, $success) {
+        $user_id = $success ? $user["id"] : 0;
+        $error_code = $success ? 200 : 404;
+        $dblog = new Table("user_log");
+        $dblog->add([
+            "user_id" => $user_id,
+            "error_code" => $error_code,
+            "log_time" => Utils::Now(),
+            "ip" => Utils::UserIPAddress(),
+            "user_agent" => $_SERVER['HTTP_USER_AGENT']
+        ]);
 
+        if ($success) {
+            # write session
+            $_SESSION["user"] = $user;
+        }
     }
 }
